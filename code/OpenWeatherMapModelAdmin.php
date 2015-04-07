2334d95c97164b28d6294232aa5f73ef987093ab
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 19:14:04 +0700

    ENHANCEMENT: Addition of model admin section

---
+++ code/OpenWeatherMapModelAdmin.php
@@ -0,0 +1,18 @@
+<?php
+
+class OpenWeatherMapModelAdmin extends ModelAdmin {
+	/**
+	 * Managed models, here weather stations
+	 */
+	private static $managed_models = array('OpenWeatherMapStation');
+
+	/* URL */
+	private static $url_segment = 'openweathermap';
+
+	/* Title of the model admin section */
+	private static $menu_title = 'Open Weather Map';
+
+	private static $menu_icon = '/openweathermap/icons/cloud310.png';
+
+
+}
