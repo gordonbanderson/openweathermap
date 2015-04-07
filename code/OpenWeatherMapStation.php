565b9d7b8a09cb6146363c58c7d431cc05775104
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-07 19:13:11 +0700

    ENHANCEMENT: Addition of nearby weather stations

--- code/OpenWeatherMapStation.php
+++ code/OpenWeatherMapStation.php
@@ -8,6 +8,23 @@ class OpenWeatherMapStation extends DataObject {
 		'Country' => 'Varchar(2)'
 	);
 
+	private static $summary_fields = array(
+		'Name' => 'Station Name',
+		'OpenWeatherMapStationID' => 'Station ID',
+		'Country' => 'Country'
+	);
+
+
+	/**
+	 * Add an index on the station id
+	 */
+	private static $indexes = array(
+		'OpenWeatherMapStationID' => true
+	);
+
+
+	/* Sort weather stations by name in the admin interfaces */
+	private static $default_sort = array('Name');
 
 	public function Forecast($days = 5) {
 		// 3hr refresh
@@ -15,30 +32,33 @@ class OpenWeatherMapStation extends DataObject {
 	}
 
 
-	public function NearByWeatherStations($prime = false) {
+	/**
+	 * Get a list of nearby weather stations and return them as SilverStripe objects.  Note this
+	 * will create records for any missing stations in the database
+	 */
+	public function NearByWeatherStations() {
 		$nearby = OpenWeatherMapAPI::nearby_weather_stations($this->Lat,$this->Lon);
-		print_r($nearby);
+		$stations = new ArrayList();
 
-		// FIXME avoid dupes
-		if ($prime) {
-			for ($i=0; $i < sizeof($nearby); $i++) {
-				error_log($i);
-				$owms = new OpenWeatherMapStation();
-				$station = $nearby[$i];
-				$stationdata = $station->station;
-				$owms->OpenWeatherMapStationID = $stationdata->id;
-				$owms->Name = $stationdata->name;
-				$coords = $stationdata->coord;
-				error_log('COORS');
-				error_log(print_r($coords,1));
-				$owms->Lat = $coords->lat;
-				$owms->Lon = $coords->lon;
-				$owms->Initialised = true;
-				$owms->write();
-			}
+		for ($i=0; $i < sizeof($nearby); $i++) {
+			$owms = new DataObject();
+
+			$station = $nearby[$i];
+			$owms->Distance = $station->distance;
+			$stationdata = $station->station;
+			$owms->OpenWeatherMapStationID = $stationdata->id;
+			$owms->Name = $stationdata->name;
+			$coords = $stationdata->coord;
+			$owms->Lat = $coords->lat;
+			$owms->Lon = $coords->lon;
+			$stations->push($owms);
 		}
 
+		$vars = new ArrayData(array(
+			'WeatherStations' => $stations
+		));
 
+		return $vars->renderWith('NearbyWeatherStations');
 	}
 
 
@@ -67,50 +87,6 @@ class OpenWeatherMapStation extends DataObject {
 			'CloudCoverPercentage' => $weather->clouds->all
 		));
 
-
-
-		/*
-		{
-    "coord": {
-        "lon": 100.51,
-        "lat": 13.86
-    },
-    "sys": {
-        "message": 0.007,
-        "country": "TH",
-        "sunrise": 1428102703,
-        "sunset": 1428147011
-    },
-    "weather": [{
-        "id": 800,
-        "main": "Clear",
-        "description": "Sky is Clear",
-        "icon": "01d"
-    }],
-    "base": "cmc stations",
-    "main": {
-        "temp": 35.658,
-        "temp_min": 35.658,
-        "temp_max": 35.658,
-        "pressure": 1017.13,
-        "sea_level": 1017.58,
-        "grnd_level": 1017.13,
-        "humidity": 37
-    },
-    "wind": {
-        "speed": 5.61,
-        "deg": 194.505
-    },
-    "clouds": {
-        "all": 0
-    },
-    "dt": 1428134922,
-    "id": 1608133,
-    "name": "Mueang Nonthaburi",
-    "cod": 200
-}
-		 */
-
 		//return $weather;
 		return $vars->renderWith('CurrentWeather');
 	}
@@ -124,7 +100,6 @@ class OpenWeatherMapStation extends DataObject {
 
 	public function prime() {
 		if (!$this->Initialised) {
-			error_log("PRIMING ".$this->OpenWeatherMapStationID);
 			$weather = OpenWeatherMapAPI::current_weather($this->OpenWeatherMapStationID);
 			$this->Lat = $weather->coord->lat;
 			$this->Lon = $weather->coord->lon;
