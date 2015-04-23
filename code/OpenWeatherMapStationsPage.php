c0922d0f3ab6dd558861530f322bdac5cbccfaca
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-04-23 16:46:04 +0700

    ENHANCEMENT: 10 day forecast populated correctly

--- code/OpenWeatherMapStationsPage.php
+++ code/OpenWeatherMapStationsPage.php
@@ -1,10 +1,9 @@
 <?php
 
-class OpenWeatherMapStationsPage extends Page {
-
+class OpenWeatherMapStationsPage extends PageWithImage {
 }
 
-class OpenWeatherMapStationsPage_Controller extends Page_Controller {
+class OpenWeatherMapStationsPage_Controller extends PageWithImage_Controller {
 	private static $allowed_actions = array(
 		'StationsMap',
 		'current',
@@ -22,6 +21,8 @@ class OpenWeatherMapStationsPage_Controller extends Page_Controller {
 		$vars = $station->TemplateVars;
 		$this->Station = $station;
 		$this->Title = 'Current Weather, '.$station->Name.', '.$station->Country;
+		$this->dataRecord->Title = $this->dataRecord->Title.' - '.$station->Name.
+			' - Current Weather';
 		return array();
 	}
 
@@ -36,6 +37,9 @@ class OpenWeatherMapStationsPage_Controller extends Page_Controller {
 		$vars = $station->TemplateVars;
 		$this->Station = $station;
 		$this->Title = 'Short Term Forecast, '.$station->Name.', '.$station->Country;
+		error_log('DATA RECORD:'.$this->dataRecord);
+		$this->dataRecord->Title = $this->dataRecord->Title.' - '.$station->Name.
+			' - Short Term Forecast';
 
 
 		Requirements::css('openweathermap/css/openweathermap.css');
@@ -55,9 +59,52 @@ JS
 			$this->httpError(404);
 		}
 		$station->DailyForecast(16,false);
+
 		$vars = $station->TemplateVars;
+
 		$this->Station = $station;
 		$this->Title = 'Long Term Forecast, '.$station->Name.', '.$station->Country;
+		$this->dataRecord->Title = $this->dataRecord->Title.' - '.$station->Name.
+			' - Long Term Forecast';
+
+
+
+		/*
+
+[0] => stdClass Object
+                (
+                    [dt] => 1429765200
+                    [temp] => stdClass Object
+                        (
+                            [day] => 24.19
+                            [min] => 21.94
+                            [max] => 26.36
+                            [night] => 22.07
+                            [eve] => 24.23
+                            [morn] => 24.19
+                        )
+
+                    [pressure] => 965.21
+                    [humidity] => 100
+                    [weather] => Array
+                        (
+                            [0] => stdClass Object
+                                (
+                                    [id] => 501
+                                    [main] => Rain
+                                    [description] => moderate rain
+                                    [icon] => 10d
+                                )
+
+                        )
+
+                    [speed] => 1.87
+                    [deg] => 124
+                    [clouds] => 92
+                    [rain] => 11.12
+                )
+
+		 */
 
 		//Requirements::css('openweathermap/css/openweathermap.css');
 		//Requirements::javascript('openweathermap/javascript/chart.min.js');
@@ -94,7 +141,7 @@ JS
 		$ck = str_replace('-', '_', $ck);
 		$ck = str_replace(' ', '_', $ck);
 
-		$map->setMarkersCacheKey($ck);
+		//$map->setMarkersCacheKey($ck);
 
 		return $map;
 	}
